dbebd6f63c8a8ab190dc9bf6e693592954c266d5
Helpful Hacker <123222498+XHackerHelperX@users.noreply.github.com>
2023-06-11 18:07:40 -0500

    OctanewOS 1.5 is here!
    Now can view the weather at your current location.
    Can now create and execute your own commands.

---
+++ OctanewOS 1.5.bat
@@ -0,0 +1,251 @@
+@echo off
+:start
+echo Octanew OS Is Starting....
+echo =============================
+goto :OS
+
+:OS
+set hour=%time:~0,2%
+if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
+
+if %hour% GEQ 0 if %hour% LEQ 11 (
+  echo Good morning!
+) else if %hour% GEQ 12 if %hour% LEQ 17 (
+  echo Good afternoon!
+) else if %hour% GEQ 18 if %hour% LEQ 23 (
+  echo Good evening!
+)
+echo Do /help for a list of commands...
+echo ============================
+
+:run_question
+echo ============================
+set /p c=Command:
+if /I "%c%" EQU "/help" goto :/help
+if /I "%c%" EQU "/ip" goto :/ip
+if /I "%c%" EQU "/date" goto :/date
+if /I "%c%" EQU "/time" goto :/time
+if /I "%c%" EQU "/reboot" goto :/reboot
+if /I "%c%" EQU "/shutdown" goto :/shutdown
+if /I "%c%" EQU "/calc" goto :/calc
+if /I "%c%" EQU "/clr" goto :/clr
+if /I "%c%" EQU "/timer" goto :/timer
+if /I "%c%" EQU "/color" goto :/color
+if /I "%c%" EQU "/randnum" goto :/randnum
+if /I "%c%" EQU "/schedule" goto :/schedule
+if /I "%c%" EQU "/lightmode" goto :/lightmode
+if /I "%c%" EQU "/weather" goto :/weather
+if /I "%c%" EQU "/cmdm" goto :/cmdm
+if /I "%c%" EQU "/runcmd" goto :/runcmd
+if /I "%c%" EQU "/%user_command_name%" goto :/custom_cmd
+echo Sorry, that is not a valid command. Type /help for a list of commands.
+goto :run_question
+
+:/help
+echo Here is the list of commands:
+echo ===============================
+echo /help: shows list of commands.
+echo /ip: shows your current IP address.
+echo /date: shows today's date.
+echo /time: shows the current time (will need to be run again to refresh clock).
+echo /reboot: reboots the operating system.
+echo /shutdown: turns off the operating system.
+echo /calc: allows you to use a built-in calculator for Octanew OS.
+echo /timer: allows you to make a custom timer in minutes.
+echo /clr: clears the screen.
+echo /color: allows you to change the color of the operating system.
+echo /randnum: generates a random number based on user input.
+echo /schedule: lets you loop certain tasks. Add "loop" after running /schedule for continuous looping of commands like /clr, /time, /ip, /date. Example: /dateloop. (must be run after running /schedule)
+echo /lightmode: allows you to change the command prompt from light to dark mode.
+echo /weather: shows you the weather of your current location.
+echo /cmdm: allows you to create a custom command and run it normally.
+goto :run_question
+
+:/ip
+ipconfig
+goto :run_question
+
+:/date
+echo %date%
+echo ============================
+goto :run_question
+
+:/time
+echo %time%
+echo ============================
+goto :run_question
+
+:/reboot
+echo Rebooting in 5 seconds...
+timeout /t 5
+start "" "%~f0"
+exit
+
+:/shutdown
+echo Shutting down in 10 seconds...
+timeout /t 10
+exit
+
+:/calc
+:menu
+echo Choose an option, 1, 2, 3, or 4:
+echo 1. Addition
+echo 2. Subtraction
+echo 3. Multiplication
+echo 4. Division
+
+set /p option=Enter option number:
+
+if "%option%"=="1" goto add
+if "%option%"=="2" goto sub
+if "%option%"=="3" goto mul
+if "%option%"=="4" goto div
+
+:add
+set /p num1=Enter the first number:
+set /p num2=Enter the second number:
+set /a result=%num1%+%num2%
+echo Result: %result%
+goto :run_question
+
+:sub
+set /p num1=Enter the first number:
+set /p num2=Enter the second number:
+set /a result=%num1%-%num2%
+echo Result: %result%
+goto :run_question
+
+:mul
+set /p num1=Enter the first number:
+set /p num2=Enter the second number:
+set /a result=%num1%*%num2%
+echo Result: %result%
+goto :run_question
+
+:div
+set /p num1=Enter the first number:
+set /p num2=Enter the second number:
+set /a result=%num1%/%num2%
+echo Result: %result%
+goto :run_question
+
+:/timer
+set /p minutes=Enter the number of minutes to countdown:
+
+set /a seconds=%minutes%*60
+
+:loop
+echo Countdown: %seconds% seconds left
+timeout /t 1 /nobreak > nul
+set /a seconds=seconds-1
+if %seconds% gtr 0 goto loop
+
+echo Time's up!
+goto :run_question
+
+:/clr
+cls
+goto :run_question
+
+:/color
+echo Choose a color:
+echo 0 = Black
+echo 1 = Blue
+echo 2 = Green
+echo 3 = Aqua
+echo 4 = Red
+echo 5 = Purple
+echo 6 = Yellow
+echo 7 = White
+echo 8 = Gray
+echo 9 = Light Blue
+echo A = Light Green
+echo B = Light Aqua
+echo C = Light Red
+echo D = Light Purple
+echo E = Light Yellow
+echo F = Bright White
+set /p color=Enter the color number/letter:
+
+color %color%
+goto :run_question
+
+:/randnum
+set /p min=Enter the minimum value:
+set /p max=Enter the maximum value:
+
+set /a range=%max%-%min%+1
+set /a num=%random% %% %range% + %min%
+
+echo Random number between %min% and %max%: %num%
+goto :run_question
+
+:/schedule
+set /p interval=Enter the interval in seconds:
+set /p task=Enter the task/command:
+
+echo Scheduling task: %task% every %interval% seconds...
+:loopinf
+timeout /t %interval% /nobreak > nul
+goto %task%
+goto :loopinf
+
+goto :run_question
+
+
+:/iploop
+ipconfig
+goto :loopinf
+
+:/dateloop
+echo %date%
+echo ============================
+goto :loopinf
+
+:/timeloop
+echo %time%
+echo ============================
+goto :loopinf
+
+:/clrloop
+cls
+goto :loopinf
+
+:/lightmode
+:menu
+echo Choose a mode:
+echo 1. Light mode
+echo 2. Dark mode
+set /p mode=
+
+if %mode%==1 goto light
+if %mode%==2 goto dark
+echo Invalid choice
+goto menu
+
+:light
+color f0
+echo Light mode enabled
+goto end
+
+:dark
+color 0f
+echo Dark mode enabled
+goto end
+
+:end
+goto :run_question
+
+:/weather
+set /p location=What is your city?: 
+curl wttr.in/%location%
+goto :run_question
+
+:/cmdm
+set /p user_command_name=Name your command ex. (example): 
+set /p user_command_do=Enter what your command will do ex. (echo example): 
+goto :run_question
+
+:/custom_cmd
+%user_command_do%
+goto :run_question
